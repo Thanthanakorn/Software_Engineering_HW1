6d8e7c3d6942057db46c66728851bd8d8c186f9b
Thanthanakorn <98535350+Thanthanakorn@users.noreply.github.com>
2023-01-19 23:24:38 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+#initiate connection
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "password"
+)
+
+
+def createMachine():
+    # TODO: Should be able to create a vending machine.
+def deleteMachine():
+    # TODO: delete vending machine.
+def editMachine():
+    # TODO: edit name, location of vending machine.
+def addProduct():
+    # TODO: There are multiple products like coke, taro, sprite.
+def editProduct():
+    # TODO: edit the amount of product.
+def removeProduct():
+    # TODO: remove product from the vending machine.
+def listing():
+    # TODO: Listing for vending machine stock products.
